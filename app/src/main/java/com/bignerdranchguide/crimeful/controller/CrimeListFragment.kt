4e78c8967be619c98f6fe0093e6a575b3b0eaf1e
kigya <kirillborichevskiy@gmail.com>
2022-01-20 20:25:54 +0300

    Update Crimes logic

--- app/src/main/java/com/bignerdranchguide/crimeful/controller/CrimeListFragment.kt
+++ app/src/main/java/com/bignerdranchguide/crimeful/controller/CrimeListFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranchguide.crimeful.R
 import com.bignerdranchguide.crimeful.extensions.log
+import com.bignerdranchguide.crimeful.model.Crime
 
 private const val TAG = "CrimeListFragment"
 
@@ -42,5 +44,29 @@ class CrimeListFragment : Fragment() {
             view.findViewById(R.id.crime_recycler_view) as RecyclerView
         crimeRecyclerView.layoutManager = LinearLayoutManager(context)
         return view
+    }
+
+    private inner class CrimeHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val titleTextView: AppCompatTextView = itemView.findViewById(R.id.crime_title)
+        val dateTextView: AppCompatTextView = itemView.findViewById(R.id.crime_date)
+    }
+
+    private inner class CrimeAdapter(var crimes: List<Crime>) :
+        RecyclerView.Adapter<CrimeHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+                : CrimeHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_crime, parent, false)
+            return CrimeHolder(view)
+        }
+
+        override fun getItemCount() = crimes.size
+
+        override fun onBindViewHolder(holder: CrimeHolder, position: Int) {
+            val crime = crimes[position]
+            holder.apply {
+                titleTextView.text = crime.title
+                dateTextView.text = crime.date.toString()
+            }
+        }
     }
 }
